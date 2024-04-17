249f3d41c41e2b90729fbb312b4f7175678aa7ec
Bilgekaan36 <b.yilmaz@workdigital.de>
2024-04-17 17:46:43 +0300

    refactor repos structure and clean overhead

--- dist/server/content-types/project-schema.js
+++ dist/server/content-types/project-schema.js
@@ -1,39 +1,39 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
-  kind: 'collectionType',
-  collectionName: 'projects',
-  info: {
-    singularName: 'project',
-    pluralName: 'projects',
-    displayName: 'Project',
-  },
-  options: {
-    draftAndPublish: false,
-  },
-  attributes: {
-    repositoryId: {
-      type: 'uid',
-      unique: true,
+    kind: 'collectionType',
+    collectionName: 'projects',
+    info: {
+        singularName: 'project',
+        pluralName: 'projects',
+        displayName: 'Project',
     },
-    name: {
-      type: 'string',
-      required: true,
-      unique: true,
+    options: {
+        draftAndPublish: false,
     },
-    description: {
-      type: 'string',
+    attributes: {
+        repositoryId: {
+            type: 'uid',
+            unique: true,
+        },
+        name: {
+            type: 'string',
+            required: true,
+            unique: true,
+        },
+        description: {
+            type: 'string',
+        },
+        repositoryUrl: {
+            type: 'string',
+        },
+        longDescription: {
+            type: 'richtext',
+        },
+        logo: {
+            type: 'media',
+            allowedTypes: ['images'],
+            multiple: false,
+        },
     },
-    repositoryUrl: {
-      type: 'string',
-    },
-    longDescription: {
-      type: 'richtext',
-    },
-    logo: {
-      type: 'media',
-      allowedTypes: ['images'],
-      multiple: false,
-    },
-  },
 };
